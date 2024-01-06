06ab8341e7071040a47773beea9ba651eb2d241b
Jacob Deepu <d2jdeepu@gmail.com>
2024-01-06 18:48:54 +0530

    Add some info to admin dashboard
        TODO: Impliment a proper UI for the dashboard with necessary data.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,6 +10,12 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     {{ __("You're logged in!") }}
+                    <ul class="list-group mt-3">
+                        <li class="list-group-item">Email: {{ Auth::user()->email }}</li>
+                        <li class="list-group-item">Referral link: {{ Auth::user()->referral_link }}</li>
+                        <li class="list-group-item">Referrer: {{ Auth::user()->referrer->name ?? 'Not Specified' }}</li>
+                        <li class="list-group-item">Refferal count: {{ count(Auth::user()->referrals) }}</li>
+                    </ul>
                 </div>
             </div>
         </div>
