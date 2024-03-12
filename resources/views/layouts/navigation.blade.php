04514cc0d5f3d392daeb577842d9953876b4db5a
Jacob Deepu <d2jdeepu@gmail.com>
2024-03-12 21:36:19 +0530

    Hide navigation links

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -11,12 +11,12 @@
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10">
                     <x-nav-link :href="route('referral.index')" :active="request()->routeIs('referral.*')">
                         {{ __('Referrals') }}
                     </x-nav-link>
@@ -71,12 +71,12 @@
 
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-        <div class="pt-2 pb-3 space-y-1">
+        <div class="hidden pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
         </div>
-        <div class="pt-2 pb-3 space-y-1">
+        <div class="hidden pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('referral.index')" :active="request()->routeIs('referral.*')">
                 {{ __('Referrals') }}
             </x-responsive-nav-link>
