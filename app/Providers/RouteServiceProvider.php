b80d4f47475969d6d2068a6d39a7e466bfd6d9ae
Jacob Deepu <d2jdeepu@gmail.com>
2024-03-12 18:33:57 +0530

    Change apllications home to referral page

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    public const HOME = '/referral';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
